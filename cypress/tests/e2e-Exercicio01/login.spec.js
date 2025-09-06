1f35be9e6fc30c43da969235ff36aaaca6b51d98
AlertaReciclagem <michelecristinapessoal@gmail.com>
2025-09-05 23:04:11 -0300

    in progresso

--- cypress/tests/e2e-Exercicio01/login.spec.js
+++ cypress/tests/e2e-Exercicio01/login.spec.js
@@ -1,49 +1,68 @@
-describe('Login - success', () => {
-  it(' login with a valid username', () => {
-    cy.visit('/signin');
-    cy.get('#username').type('testuser');   
-    cy.get('#password').type('secret');     
-    cy.get('[data-test="signin-submit"]').click();
-  });
-});
+// Defina o selectorList fora do describe para usar em todos os testes
+const selectorList = {
+  firstnameInput: "[name='firstName']",
+  usernameInput: "[name='username']",  
+  lastnameInput: "[name='lastName']",
+  passwordInput: "[name='password']",
+  signinButton: '[data-test="signin-submit"]',
+  signupButton: '[data-test="signup-submit"]', 
+  errorMessage: '[data-test="signin-error"]',
+  confirmPasswordInput: "[name='confirmPassword']",
+};
 
-describe('Login - invalid', () => {
+describe('RWA - Real World App - Login valido, invalid', () => {
+
   it('Display error message when using invalid credentials', () => {
     cy.visit('/signin');
-    cy.get('#username').type('usuario_invalido');
-    cy.get('#password').type('senha_errada');
-    cy.get('[data-test="signin-submit"]').click();
-    cy.get('[data-test="signin-error"]').should('be.visible');
-    cy.get('[data-test="signin-error"]').should('contain', 'Username or password is invalid');
+    cy.get(selectorList.usernameInput).type('testerror');
+    cy.get(selectorList.passwordInput).type('error123');
+    cy.get(selectorList.signinButton).click();
+    cy.get(selectorList.errorMessage).should('be.visible');
+    cy.get(selectorList.errorMessage).should('contain', 'Username or password is invalid');
   });
-});
 
-describe('New user registration', () => {
-  it(' Register a new user with valid information', () => {
+  it('Error messages displayed when trying to register without filling in required fields', () => {
     cy.visit('/signup');
-    cy.get('#firstName').type('Novo');
-    cy.get('#lastName').type('Usuário');
-    cy.get('#username').type(`usuario${Date.now()}`);
-    cy.get('#password').type('senha123');
-    cy.get('#confirmPassword').type('senha123');
-    cy.get('[data-test="signup-submit"]').click();
-    cy.url().should('include', '/signup'); 
+    cy.get(selectorList.signupButton).click();
+    cy.get(selectorList.usernameInput).should('be.visible');
+    cy.get(selectorList.lastnameInput).should('be.visible');
+    cy.get(selectorList.passwordInput).should('be.visible');
+    cy.get(selectorList.confirmPasswordInput).should('be.visible');
   });
-});
 
-describe('Registration with incomplete information', () => {
-  it(' Error messages displayed when trying to register without filling in required fields', () => {
+  it('Register a new user with valid information', () => {
     cy.visit('/signup');
-    cy.get('[data-test="signup-submit"]').click();
+    cy.get(selectorList.firstnameInput).type('Gabriel');
+    cy.get(selectorList.lastnameInput).type('Scoot');
+    cy.get(selectorList.usernameInput).type('GabrielScoot_'); // faltava username
+    cy.get(selectorList.passwordInput).type('teste123');
+    cy.get(selectorList.confirmPasswordInput).type('teste123');
+    cy.get(selectorList.signupButton).click();
+    cy.url().should('include', '3000/'); 
+  });
 
-    cy.get("[name='firstName']");
-    cy.get("[name='lastName']").should('be.visible');
-    cy.get("[name='username']").should('be.visible');
-    cy.get("[name='password']").should('be.visible');
-    cy.get("[name='confirmPassword']").should('be.visible');
+  it('Login with a valid username', () => {
+    cy.visit('/signin');
+    cy.get(selectorList.usernameInput).type('GabrielScoot_');   
+    cy.get(selectorList.passwordInput).type('teste123');     
+    cy.get(selectorList.signinButton).click();
+    cy.url().should('include', '3000/');
   });
 });
 
 
-
-
+// dashboard
+describe('Login - success', () => {
+  it('should login and redirect to dashboard', () => {
+    cy.visit('/signin');
+    cy.get(selectorList.usernameInput).type('GabrielScoot_');   
+    cy.get(selectorList.passwordInput).type('teste123');     
+    cy.get(selectorList.signinButton).click();
+    cy.url().should('include', '3000/');
+    
+    cy.get("[name='bankName']").type('Banco do Brasil');   
+    cy.get("[name='routingNumber']").type('987654321');
+    cy.get("[name='accountNumber']").type('123456789');    
+    cy.get(".css-flggza-MuiGrid-root").click();
+  });
+});
